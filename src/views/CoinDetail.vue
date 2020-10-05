6ef5e84e89b5d1c475ff62bbdd4e3a05541d30ec
Karla Agraz <karla.agraz@gmail.com>
2020-10-04 23:12:40 -0500

    Utilizar componentes de terceros

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -65,6 +69,14 @@
           <span class="text-xl"></span>
         </div>
       </div>
+
+      <line-chart
+        class="my-10"
+        :colors="['orange']"
+        :min="min"
+        :max="max"
+        :data="chartData"
+      />
     </template>
   </div>
 </template>
@@ -76,6 +88,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       asset: {},
       history: []
     }
@@ -99,6 +112,14 @@ export default {
         this.history.reduce((a, b) => a + parseFloat(b.priceUsd), 0) /
         this.history.length
       )
+    },
+
+    chartData() {
+      const data = []
+      this.history.map(h => {
+        data.push([h.date, parseFloat(h.priceUsd).toFixed(2)])
+      })
+      return data
     }
   },
 
@@ -109,12 +130,13 @@ export default {
   methods: {
     getCoin() {
       const id = this.$route.params.id
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      this.isLoading = true
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+        .then(([asset, history]) => {
           this.asset = asset
           this.history = history
-        }
-      )
+        })
+        .finally(() => (this.isLoading = false))
     }
   }
 }
